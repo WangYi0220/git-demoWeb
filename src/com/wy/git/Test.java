7fbd279324b766b1d5a699348a3d4316f1408a8c
YiWang <1124991320@qq.com>
2019-03-07 14:59:05 +0800

    调整格式

--- src/com/wy/git/Test.java
+++ src/com/wy/git/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Github");
-                System.out.println("Update for Github");
+		System.out.println("Update for Github");
 	}
 }
